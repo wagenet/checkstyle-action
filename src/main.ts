18530f2f40d9bd9e0c041a136441fa0a362e496f
Peter Wagenet <peter.wagenet@gmail.com>
2020-06-25 10:12:41 -0700

    Fixes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
-import * as path from 'path';
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const matchersPath = path.join(__dirname, '..', '.github');
-    console.log(`##[add-matcher]${path.join(matchersPath, 'checkstyle.json')}`);
+    const matchersPath = path.join(__dirname, '..', '.github')
+    console.log(`##[add-matcher]${path.join(matchersPath, 'checkstyle.json')}`)
   } catch (error) {
     core.setFailed(error.message)
   }
